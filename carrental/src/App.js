38b5d2dab5299e36db4249ea7c7f4480094db624
Sugyeong <onmay16@gmail.com>
2022-10-17 14:07:37 -0500

    update

--- carrental/src/App.js
+++ carrental/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 import React, { useState, useEffect } from "react";
 import DatePicker, { registerLocale } from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+// import "react-datepicker/dist/react-datepicker.css";
 import ko from 'date-fns/locale/ko';
 
 import Container from 'react-bootstrap/Container';
@@ -15,35 +15,65 @@ registerLocale('ko', ko);
 
 function App() {
 
-  const [now, setNow] = useState(new Date())
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+
+  const [minTime, setMinTime] = useState(new Date());
+  const [maxTime, setMaxTime] = useState(new Date());
+  const [excludedTime, setExcludedTime] = useState([]);
+
   const [days, setDays] = useState(0);
   const [hours, setHours] = useState(0);
+  const [selected, setSelected] = useState(false);
 
   function dateCalculator(startDate, endDate) {
-    const totalHours = Math.ceil((endDate - startDate) / (1000 * 3600))
+    const totalHours = (endDate - startDate) / (1000 * 3600)
     const calculatedDays = Math.floor(totalHours / 24)
-    const calculatedHours = totalHours % 24
-    setDays(calculatedDays)
-    setHours(calculatedHours)
-    // setDays(calculatedDays)
-    // if ((calculatedHours !== 0 && calculatedHours < 7 && calculatedDays === 0) || ()) {
-    //   // alert("렌트 일정을 최소 7시간 이상 설정해 주세요.")
-    //   setHours(0);
-    // } else {
-    //   setHours(calculatedHours)
-    // }
+    const calculatedHours = (totalHours % 24).toFixed(1);
+
+    if (calculatedHours >= 7) {
+      setDays(calculatedDays+1);
+      setHours(0);
+    } else {
+      setDays(calculatedDays)
+      setHours(calculatedHours)
+    }
   }
 
   useEffect(() => {
+    startDate.setDate(startDate.getDate()+2);
+    startDate.setHours(10);
     startDate.setMinutes(0);
-    endDate.setMinutes(0);
+
     endDate.setDate(startDate.getDate()+2);
+    endDate.setHours(10);
+    endDate.setMinutes(0);
+
+    minTime.setHours(8);
+    minTime.setMinutes(0);
+
+    maxTime.setHours(18);
+    maxTime.setMinutes(30);
+
+    const notAvailableTime = []
+    for (let count = 3; count < 8; count += 0.5) {
+      let time = new Date()
+      if (count % 1 !== 0) {
+        time.setHours(Math.floor(count), 30, 0);
+      } else {
+        time.setHours(Math.floor(count), 0, 0);
+      }
+      notAvailableTime.push(time);
+    };
+    setExcludedTime(notAvailableTime);
   }, [])
 
   useEffect(() => {
     dateCalculator(startDate, endDate)
+    if (days === 0 && hours < 7 && selected) {
+      alert("렌트 시간을 7시간 이상으로 설정해 주세요.");
+      endDate.setDate(startDate.getDate()+1);
+    }
   }, [startDate, endDate])
 
   useEffect(() => {
@@ -51,16 +81,7 @@ function App() {
       alert("반납 일자를 픽업 일자 이후로 설정해 주세요.")
       setEndDate(startDate)
     }
-  }, [startDate])
-
-  // useEffect(() => {
-  //   if (startDate > endDate) {
-  //     alert("반납 일자를 픽업 일자 이후로 설정해 주세요.")
-  //     setEndDate(startDate)
-  //   }
-  // }, [endDate])
-  
-
+  }, [endDate])
 
   return (
     <div className="App">
@@ -74,80 +95,91 @@ function App() {
           </Navbar.Brand>
         </Container>
       </Navbar>
-      <div style={{ display: 'flex', marginTop: '20px' }}>
-        {/* actual body */}
-        <div style={{ display: 'flex', flexDirection: 'row', margin: 'auto' }}>
-          <div style={{ marginRight: '50px' }}>
-            {/* date picking table */}
-            <ListGroup>
-              <ListGroup.Item style={{ paddingTop:'12px', paddingBottom:'12px', paddingLeft:'20px', paddingRight:'20px' }}>
-                <h6>픽업일자</h6>
-                <DatePicker
-                  selected={startDate}
-                  onChange={(date) => {
-                    setStartDate(date)
-                  }}
-                  minDate={now}
-                  dateFormat="yyyy-MM-dd"
-                  locale='ko' />
-              </ListGroup.Item>
-              <ListGroup.Item style={{ paddingTop:'12px', paddingBottom:'12px', paddingLeft:'20px', paddingRight:'20px' }}>
-                <h6>픽업시간</h6>
-                <DatePicker
-                  selected={startDate}
-                  onChange={(date) => setStartDate(date)}
-                  showTimeSelect
-                  showTimeSelectOnly
-                  timeIntervals={30}
-                  timeCaption="시간"
-                  dateFormat="h:mm aa"
-                  locale='ko' />
-              </ListGroup.Item>
-              <ListGroup.Item style={{ paddingTop:'12px', paddingBottom:'12px', paddingLeft:'20px', paddingRight:'20px' }}>
-                <h6>반납일자</h6>
-                <DatePicker
-                  selected={endDate}
-                  onSelect={(date) => {
-                    setEndDate(date)
-                  }}
-                  minDate={startDate}
-                  dateFormat="yyyy-MM-dd"
-                  locale='ko' />
-              </ListGroup.Item>
-              <ListGroup.Item style={{ paddingTop:'12px', paddingBottom:'12px', paddingLeft:'20px', paddingRight:'20px' }}>
-                <h6>반납시간</h6>
-                <DatePicker
-                  selected={endDate}
-                  onChange={(date) => setEndDate(date)}
-                  showTimeSelect
-                  showTimeSelectOnly
-                  timeIntervals={30}
-                  timeCaption="시간"
-                  dateFormat="h:mm aa"
-                  locale='ko' />
-              </ListGroup.Item>
-              <ListGroup.Item style={{ backgroundColor: '#F5F5F5', paddingTop:'12px', paddingBottom:'12px', paddingLeft:'20px', paddingRight:'20px' }}>
-                <h6>렌트기간</h6>
-                <div style={{ fontWeight:'800' }}>{days}일 {(hours && days<3) ? <span>+ {hours}시간</span> : null}</div>
-              </ListGroup.Item>
-            </ListGroup>
-          </div>
-          {/* tables (right section) */}
+      <div className='body-section'>
+        <div className='body-row'>
+          <ListGroup style={{ marginRight: '50px' }}>
+            <ListGroup.Item className='list-group-item'>
+              <h6>픽업일자</h6>
+              <DatePicker
+                className='date-picker'
+                selected={startDate}
+                onChange={(date) => {
+                  setStartDate(date);
+                }}
+                minDate={startDate}
+                dateFormat=" yyyy-MM-dd"
+                locale='ko' />
+            </ListGroup.Item>
+            <ListGroup.Item className='list-group-item'>
+              <h6>픽업시간</h6>
+              <DatePicker
+                className='date-picker'
+                selected={startDate}
+                onChange={(date) => {
+                  setStartDate(date);
+                  setSelected(true);
+                }}
+                showTimeSelect
+                showTimeSelectOnly
+                dateFormat=" HH:mm"
+                minTime={minTime}
+                maxTime={maxTime}
+                timeIntervals={30}
+                timeCaption="시간"
+                locale='ko' />
+            </ListGroup.Item>
+            <ListGroup.Item className='list-group-item'>
+              <h6>반납일자</h6>
+              <DatePicker
+                className='date-picker'
+                selected={endDate}
+                onSelect={(date) => {setEndDate(date)}}
+                minDate={startDate}
+                dateFormat=" yyyy-MM-dd"
+                locale='ko' />
+            </ListGroup.Item>
+            <ListGroup.Item className='list-group-item'>
+              <h6>반납시간</h6>
+              <DatePicker
+                className='date-picker'
+                selected={endDate}
+                onChange={(date) => {
+                  setEndDate(date)
+                  setSelected(true)
+                }}
+                showTimeSelect
+                showTimeSelectOnly
+                timeIntervals={30}
+                excludeTimes={excludedTime}
+                timeCaption="시간"
+                dateFormat=" HH:mm"
+                locale='ko' />
+            </ListGroup.Item>
+            <ListGroup.Item className='list-group-item' style={{ backgroundColor: '#F5F5F5' }}>
+              <h6>렌트기간</h6>
+              <div style={{ fontWeight:'800' }}>
+                {days}일
+                {(hours>0.5 && hours<7 && days<3) ?<span>+ {Math.floor(hours)}시간</span> : null}
+                {(hours && hours<7 && days<3 && hours%1 !== 0 ? <span> 30분</span>:null)}
+              </div>
+            </ListGroup.Item>
+          </ListGroup>
+          <br />
           <div style={{ margin: 'auto' }}>
-            <div style={{ textAlign:'left' }}>
+            <div style={{ textAlign:'left', color: 'red', fontWeight:600 }}>
               <div>* 견적계산기는 렌트 비용 참고용입니다.</div>
-              <div>* 자세한 내용은 드림렌트카 예약안내 에서 확인해주세요.</div>
-              <div>* 렌트기간 30일 이상의 장기렌트일 경우 메일로 별도 문의 바랍니다. tillpark3@naver.com</div>
+              <div>* 자세한 내용은 <a href='https://cafe.naver.com/guamfree/335157'>드림렌트카 예약안내</a>에서 확인해주세요.</div>
+              <div>* 렌트기간 30일 이상의 장기렌트일 경우 메일로 별도 문의 바랍니다. &lt;<a href='mailto:tillpark3@naver.com'>tillpark3@naver.com</a>&gt;</div>
             </div>
             <br />
             <PriceTable
               category="컴팩트"
               cars="미츠비, 미라지"
-              reservePrice={10}
-              onSitePay={45}
-              onSitePayInsurance={63}
-              defaultPrice={55}
-              defaultPriceInsurance={73}
+              reservePrice={10} // 예약금액
+              onSitePay={45} // 현장지불
+              onSitePayInsurance={63} // 현장지불 (+보험)
+              defaultPrice={55} //기본요금
+              defaultPriceInsurance={73} //기본요금 (+보험)
               days={days}
               hours={hours} />
             <PriceTable
@@ -193,6 +225,7 @@ function App() {
           </div>
         </div>
       </div>
+      <footer style={{ backgroundColor:'#343A3F', color:'#ffffff', paddingTop:'16px', paddingBottom:'16px', fontWeight:600 }}>Copyright © 괌자길 2020</footer>
     </div>
   );
 }
--- carrental/src/PriceTable.js
+++ carrental/src/PriceTable.js
@@ -1,26 +1,18 @@
-import React, { useState } from 'react'
-
 import Table from 'react-bootstrap/Table';
 
 function PriceTable(props) {
-
-  const [defaultPrice, setDefaultPrice] = useState(0)
-  const [plusInsurancePrice, setplusInsurancePrice] = useState(0)
-
-  function calculator(reservePrice, onSitePay, onSitePayInsurance, defaultPrice, defaultPriceInsurance, days, hours) {
-
-  }
-
+  
   return (
     <div>
       <hr />
       <br />
-      <h4>
+      <h3 style={{ textAlign:'left', fontWeight:500 }}>
         {props.category}&nbsp;
-        <small>{props.cars}</small>
-      </h4>
+        <small style={{ fontSize:'14px', color:'#A5A5A5', fontWeight:600 }}>{props.cars}</small>
+      </h3>
+      <br />
       <h6>카페요금</h6>
-      <Table bordered>
+      <Table bordered style={{ fontSize: '13px' }}>
         <thead>
           <tr style={{ backgroundColor: '#F5F5F5' }}>
             <th colSpan={2}>기본요금</th>
@@ -44,9 +36,9 @@ function PriceTable(props) {
         <tbody>
           <tr>
             <td>${props.reservePrice*props.days}</td>
-            <td>${props.onSitePay*props.days}</td>
+            <td>${props.onSitePay*props.days+10*props.hours}</td>
             <td>${props.reservePrice*props.days}</td>
-            <td>${props.onSitePayInsurance*props.days}</td>
+            <td>${props.onSitePayInsurance*props.days+10*props.hours}</td>
           </tr>
         </tbody>
       </Table>
